32e8f72db556d87fa4bb6b6cfb5ad5e174beabb9
Korey <kkinney2@student.fitchburgstate.edu>
2019-09-18 15:17:21 -0400

    Tile Script

---
+++ Riverhill - Unity/Assets/Scripts/Tile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    Grid grid;
+
+    Tile[] neighbors;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GetNeighbors()
+    {
+        neighbors = new Tile[6];
+
+        neighbors[0] = (grid.GetComponent<GridLayout>().LocalToCell + new Vector3 (1,0,0));
+    }
+}
