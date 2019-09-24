1e4c15b8e3c6526d8887a481fdbc9f5339430dfa
Korey Kinney <kkinney2@student.fitchburgstate.edu>
2019-09-24 09:40:57 -0400

    Menu Script

---
+++ Riverhill - Unity/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void newGame()
+    {
+        SceneManager.LoadScene("_MainScene_");
+    }
+
+    public void loadGame()
+    {
+        //no scene or existing games to load rn
+        Debug.Log("Load Game");
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit Game");
+    }
+
+}
