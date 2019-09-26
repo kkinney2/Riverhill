e9d1529bbcfdefc75ed339304915a77d7ad4e42f
Korey Kinney <kkinney2@student.fitchburgstate.edu>
2019-09-26 12:31:06 -0400

    Actor Movement Script
    Currently used for testing: INCOMPLETE

--- Riverhill - Unity/Assets/Scripts/ActorController.cs
+++ Riverhill - Unity/Assets/Scripts/ActorController.cs
@@ -7,20 +7,19 @@ public class ActorController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(Move());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     IEnumerator Move()
     {
         bool hasPath = false;
-        List<Tile> path;
-        RaycastHit2D[] results;
+        List<Tile> path = new List<Tile>();
 
         while (!hasPath)
         {
@@ -28,9 +27,29 @@ public class ActorController : MonoBehaviour
             {
                 if (Physics.Raycast(Input.mousePosition + Vector3.back, Vector3.forward, Mathf.Infinity, 1))
                 {
+                    Vector3Int cellPos = TileManager.Instance.grid.WorldToCell(Input.mousePosition);
 
+                    path = TileManager.Instance.FindPath(transform.position, TileManager.Instance.grid.CellToWorld(cellPos));
+                    hasPath = true;
+                }
+            }
+        }
 
-                    TileManager.Instance.FindPath(transform.position, )
+        bool hasReachedTarget = false;
+
+        while (!hasReachedTarget)
+        {
+            for (int i = 0; i < path.Count; i++)
+            {
+                // Move our position a step closer to the target.
+                float step = speed * Time.deltaTime; // calculate distance to move
+                transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+
+                // Check if the position of the cube and sphere are approximately equal.
+                if (Vector3.Distance(transform.position, target.position) < 0.001f)
+                {
+                    // Swap the position of the cylinder.
+                    target.position *= -1.0f;
                 }
             }
         }
