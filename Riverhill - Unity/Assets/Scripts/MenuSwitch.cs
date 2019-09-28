cc7497096ba3c79da587d47f970a1518b0b39dc5
emwirthdev <emwirthdev@users.noreply.github.com>
2019-09-28 16:46:07 -0400

    More menu fixes
    Fixes for arrow keys nav. vs. mouse click

---
+++ Riverhill - Unity/Assets/Scripts/MenuSwitch.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MenuSwitch : MonoBehaviour
+{
+
+    public GameObject offCanvas;
+    public GameObject onCanvas;
+    public GameObject firstObj;
+
+    public void Switch()
+    {
+
+        offCanvas.SetActive(true);
+        onCanvas.SetActive(false);
+        GameObject.Find("EventSystem").GetComponent<EventSystem>().SetSelectedGameObject(firstObj, null);
+
+    }
+
+}
---
+++ Riverhill - Unity/Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class OptionsMenu : MonoBehaviour
+{
+
+    public EventSystem eventSys;
+    private GameObject storeSelect;
+
+    public void Start()
+    {
+        storeSelect = eventSys.firstSelectedGameObject;
+    }
+
+    public void Update()
+    {
+        if (eventSys.currentSelectedGameObject != storeSelect)
+        {
+            if (eventSys.currentSelectedGameObject == null)
+            {
+                eventSys.SetSelectedGameObject(storeSelect);
+            }
+
+            else
+            {
+                storeSelect = eventSys.currentSelectedGameObject;
+            }
+        }
+    }
+
+}
