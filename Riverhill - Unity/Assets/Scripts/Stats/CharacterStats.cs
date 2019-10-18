0354451667862b664701eb4cb5648d26b5be03f9
Unknown <kkinney2@student.fitchburgstate.edu>
2019-10-18 17:15:40 -0400

    Created CharacterStats
    Merged playerstats and enemy stats into characterstats, only difference is that characterstats has a bool isEnemy

---
+++ Riverhill - Unity/Assets/Scripts/Stats/CharacterStats.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CharacterStats
+{
+    public bool isEnemy;
+
+    //enemy base stats
+    public string Name;
+
+    public float BaseHP;
+    public float CurrentHP;
+}
