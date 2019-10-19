78a28c4bdc4a76bd8026a41e31251d3305981ff4
Unknown <kkinney2@student.fitchburgstate.edu>
2019-10-19 13:27:01 -0400

    Idle State Created
    Serves as a switching point where it doesn't perform anything besides if there's too many actions. This may be changed if we want to end turns based on "EndTurn" button

---
+++ Riverhill - Unity/Assets/Scripts/TurnbasedScripts/States/Idle.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* 
+ * Can be used to "Idle" a Character so they they don't accidently
+ * use a state while switching characters or states
+ */
+
+public class Idle : IState
+{
+    CharacterState characterState;
+    private BattleStateMachine characterStateMachine;
+
+    BattleManager battleManager;
+
+    public Idle(CharacterState a_CharacterState, BattleStateMachine a_BattleStateMachine)
+    {
+        this.characterState = a_CharacterState;
+        this.characterStateMachine = a_BattleStateMachine;
+    }
+
+    public void Enter()
+    {
+        Debug.Log("Entering Idle State");
+        battleManager = BattleManager.Instance;
+    }
+
+    public void Execute()
+    {
+        //Debug.Log("Executing Idle State");
+        if (characterState.actionCount >= GameSettings.Instance.MaxActionCount)
+        {
+            battleManager.nextCharacter = true;
+
+            // Exiting/switching character states already does this
+            //characterState.actionCount = 0;
+        }
+    }
+
+    public void Exit()
+    {
+        Debug.Log("Exiting Idle State");
+    }
+}
