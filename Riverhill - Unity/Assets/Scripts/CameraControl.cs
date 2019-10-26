73676cfb5401ac7a62a09506e3ae8776fb42f1ff
emwirthdev <emwirthdev@users.noreply.github.com>
2019-10-26 15:47:41 -0400

    Camera zoom
    working on scroll wheel zoom

--- Riverhill - Unity/Assets/Scripts/CameraControl.cs
+++ Riverhill - Unity/Assets/Scripts/CameraControl.cs
@@ -4,21 +4,33 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
+    public bool zoomInLimit;
+    public bool zoomOutLimit;
     //center camera on active player
 
     //zoom function, centered on active player
-    
+
     //want camera to follow the active player as they move
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            //GetComponent<Camera>().fieldOfView--;
+            GetComponent<Camera>().orthographicSize -= 1f;
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            //GetComponent<Camera>().fieldOfView++;
+            GetComponent<Camera>().orthographicSize += 1f;
+        }   
     }
 }
