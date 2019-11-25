8f076f45bab78b2884439c3349d7464b6ace6c96
emwirthdev <emwirthdev@users.noreply.github.com>
2019-11-25 15:35:30 -0500

    update menu sounds
    heeyaw

--- Riverhill - Unity/Assets/Scripts/MenuScripts/MainMenu.cs
+++ Riverhill - Unity/Assets/Scripts/MenuScripts/MainMenu.cs
@@ -49,7 +49,7 @@ public class MainMenu : MonoBehaviour
     private IEnumerator NewGameButtonDelay()
     {
         //Debug.Log(Time.time);
-        yield return new WaitForSeconds(3.5f);
+        yield return new WaitForSeconds(3f);
         //Debug.Log(Time.time);
         SceneManager.LoadScene("CutScene");
     }
