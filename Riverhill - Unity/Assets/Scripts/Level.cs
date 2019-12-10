06cbee9ce19425ca4945ae35332942ec3690a430
Korey Kinney <kkinney2@student.fitchburgstate.edu>
2019-12-10 14:03:04 -0500

    Level
    Small update to Level, mostly switching from coroutine to normal function.

--- Riverhill - Unity/Assets/Scripts/Level.cs
+++ Riverhill - Unity/Assets/Scripts/Level.cs
@@ -27,27 +27,39 @@ public class Level : MonoBehaviour
 
     public void Load()
     {
-        //StartCoroutine(LoadLevel(true));
-        Grid_Tile.gameObject.SetActive(true);
-        Grid_Obstacles.gameObject.SetActive(true);
-        Grid_Unwalkable.gameObject.SetActive(true);
-
-        TileManager.Instance.grid = Grid_Tile.GetComponent<Grid>();
-        TileManager.Instance.Reset();
-        BattleManager.Instance.isLevelLoaded = true;
+        LoadLevel(true);
     }
 
     public void Unload()
     {
-        StartCoroutine(LoadLevel(false));
+        LoadLevel(false);
+        //StartCoroutine(Coroutine_LoadLevel(false));
     }
 
-    IEnumerator LoadLevel(bool toggle)
+    public void LoadLevel(bool toggle)
     {
         BattleManager.Instance.currentLevel = this;
 
         Grid_Tile.gameObject.SetActive(toggle);
-        yield return new WaitUntil(() => Grid_Tile.gameObject.activeSelf == toggle); 
+        Grid_Obstacles.gameObject.SetActive(toggle);
+        Grid_Unwalkable.gameObject.SetActive(toggle);
+
+        if (toggle)
+        {
+            TileManager.Instance.grid = Grid_Tile.GetComponent<Grid>();
+            TileManager.Instance.Reset();
+        }
+
+        BattleManager.Instance.isLevelLoaded = toggle;
+
+    }
+
+    IEnumerator Coroutine_LoadLevel(bool toggle)
+    {
+        BattleManager.Instance.currentLevel = this;
+
+        Grid_Tile.gameObject.SetActive(toggle);
+        yield return new WaitUntil(() => Grid_Tile.gameObject.activeSelf == toggle);
 
         Grid_Obstacles.gameObject.SetActive(toggle);
         yield return new WaitUntil(() => Grid_Obstacles.gameObject.activeSelf == toggle);
@@ -55,9 +67,13 @@ public class Level : MonoBehaviour
         Grid_Unwalkable.gameObject.SetActive(toggle);
         yield return new WaitUntil(() => Grid_Unwalkable.gameObject.activeSelf == toggle);
 
-        TileManager.Instance.grid = Grid_Tile.GetComponent<Grid>();
-        TileManager.Instance.Reset();
-        BattleManager.Instance.isLevelLoaded = true;
+        if (toggle)
+        {
+            TileManager.Instance.grid = Grid_Tile.GetComponent<Grid>();
+            TileManager.Instance.Reset();
+        }
+
+        BattleManager.Instance.isLevelLoaded = toggle;
 
         yield break;
     }
